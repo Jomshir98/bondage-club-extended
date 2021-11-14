12465abf87cb3727e0f631a2546b4a011d19bf6a
Jomshir98 <jomshir98@protonmail.com>
2021-11-14 22:34:51 +0000

    Deploying to gh-pages from @ Jomshir98/bondage-club-extended@5a0b5b3b0930857d53ce3284cc332f2b15bdd768 🚀

--- devel/bcxLoader.user.js
+++ devel/bcxLoader.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         BCX - Bondage Club Extended (Loader)
 // @namespace    BCX
-// @version      1.0.3
+// @version      2.0.3
 // @description  Loader of Jomshir's "Bondage Club Extended" mod
 // @author       Jomshir98
 // @include      /^https://www\.bondageprojects\.elementfx\.com/R\d+/(BondageClub|\d+)(/(index.html)?)?$
 // @include      /^https://(www\.)?bondage-europe\.com/R\d+/(BondageClub|\d+)(/(index.html)?)?$
 // @homepage     https://github.com/jomshir98/bondage-club-extended#readme
 // @source       https://github.com/jomshir98/bondage-club-extended
-// @downloadURL  https://jomshir98.github.io/bondage-club-extended/bcxLoader.user.js
+// @downloadURL  https://jomshir98.github.io/bondage-club-extended/devel/bcxLoader.user.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
@@ -19,7 +19,7 @@ setTimeout(
 			let n = document.createElement("script");
 			n.setAttribute("language", "JavaScript");
 			n.setAttribute("crossorigin", "anonymous");
-			n.setAttribute("src", "https://jomshir98.github.io/bondage-club-extended/bcx.js?_=" + Date.now());
+			n.setAttribute("src", "https://jomshir98.github.io/bondage-club-extended/devel/bcx.js?_=" + Date.now());
 			n.onload = () => n.remove();
 			document.head.appendChild(n);
 		}

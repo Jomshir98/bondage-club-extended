aa610d280ca840f4a89acdd239eb9ac36f5730ba
Jomshir98 <jomshir98@protonmail.com>
2021-11-19 21:12:21 +0000

    Deploying to gh-pages from @ Jomshir98/bondage-club-extended@7a8519865e73e0cbca7519ef051eb5f010d6c213 🚀

--- devel/bcxLoader.user.js
+++ devel/bcxLoader.user.js
@@ -4,8 +4,8 @@
 // @version      2.0.4
 // @description  Loader of Jomshir's "Bondage Club Extended" mod
 // @author       Jomshir98
-// @include      /^https://www\.bondageprojects\.elementfx\.com/R\d+/(BondageClub|\d+)(/(index.html)?)?$/
-// @include      /^https://(www\.)?bondage-europe\.com/R\d+/(BondageClub|\d+)(/(index.html)?)?$/
+// @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
+// @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @homepage     https://github.com/jomshir98/bondage-club-extended#readme
 // @source       https://github.com/jomshir98/bondage-club-extended
 // @downloadURL  https://jomshir98.github.io/bondage-club-extended/devel/bcxLoader.user.js

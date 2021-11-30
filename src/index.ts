3c198758a9eac04e044fc6cad9af2c7c1c2c3561
jomshir98 <jomshir98@protonmail.com>
2021-11-30 19:18:18 +0000

    Fix BCX loading on failed login

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { InfoBeep } from "./utilsClub";
 import { hookFunction } from "./patching";
 import { init, loginInit } from "./main";
+import { isObject } from "./utils";
 
 import "./modules";
 
@@ -8,7 +9,10 @@ async function initWait() {
 	if (CurrentScreen == null || CurrentScreen === "Login") {
 		hookFunction("LoginResponse", 0, (args, next) => {
 			next(args);
-			loginInit(args[0]);
+			const response = args[0];
+			if (isObject(response) && typeof response.Name === "string" && typeof response.AccountName === "string") {
+				loginInit(args[0]);
+			}
 		});
 		InfoBeep(`BCX Ready!`);
 		console.log(`BCX Ready!`);

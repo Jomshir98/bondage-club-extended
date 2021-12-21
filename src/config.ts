73494e81f9353fe38b1ef2813727e29e6d557d01
jomshir98 <jomshir98@protonmail.com>
2021-12-21 21:12:06 +0000

    Update R75 ChatSearchLoad hash

--- src/config.ts
+++ src/config.ts
@@ -56,7 +56,7 @@ export const FUNCTION_HASHES: Record<string, string[]> = {
 	ChatRoomSyncMemberLeave: ["A95EADE6"],
 	ChatRoomUpdateDisplay: ["8DFC494A"],
 	ChatSearchJoin: ["22514B80"],
-	ChatSearchLoad: ["F5CC9CAE"],
+	ChatSearchLoad: ["F5CC9CAE", "8AF12D1C"],
 	ChatSearchNormalDraw: ["6BEDBABB"],
 	ChatSearchRun: ["4C56AC68", "06BFF877", "7909670E"],
 	CheatFactor: ["594CFC45"],

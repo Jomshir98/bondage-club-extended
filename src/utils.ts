f09ac9a33dfda8e0289b960686bdb66f9845c975
jomshir98 <jomshir98@protonmail.com>
2022-03-07 21:20:13 +0000

    Add shuffleArray utility function

--- src/utils.ts
+++ src/utils.ts
@@ -238,3 +238,14 @@ export const BCX_VERSION_PARSED: Readonly<BCXVersion> = (() => {
 	}
 	return res;
 })();
+
+/**
+ * Shuffles an array in-place
+ * @param array The array to shuffle
+ */
+export function shuffleArray(array: any[]) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+}

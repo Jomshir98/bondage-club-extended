94edfd9053603f2b010066d8a29e13eb1b586044
kinksideofmoon <pawel.wojtal@gmail.com>
2024-08-02 11:35:52 +0200

    Fix logic of the loop through array with forbidden mods

--- src/main.ts
+++ src/main.ts
@@ -84,11 +84,11 @@ export function init() {
 	}
 
 	//#endregion
-	console.info("CHECKING ENABLED MODULES AGAINST FORBIDDEN LIST");
+	console.log("CHECKING ENABLED MODULES AGAINST FORBIDDEN LIST");
 
 	const enabledForbiddenMods: string[] = detectForbiddenOtherMods();
 
-	console.info("Found " + enabledForbiddenMods.length + " enabled forbidden modules.");
+	console.log("Found " + enabledForbiddenMods.length + " enabled forbidden modules.");
 
 	if (enabledForbiddenMods.length>0) {
 		alert("Found forbidden BC modules. Please disable them first!");

3fc6616167a04c098280a8ed21d7672cf01930e1
Jean-Baptiste Emmanuel Zorg <zorgjeanbe@proton.me>
2025-08-01 18:58:28 +0200

    Update the loader with the Asia server

--- static_stable/bcxLoader.user.js
+++ static_stable/bcxLoader.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         BCX - Bondage Club Extended (Loader)
 // @namespace    BCX
-// @version      1.0.5
+// @version      1.0.6
 // @description  Loader of Jomshir's "Bondage Club Extended" mod
 // @author       Jomshir98
 // @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
+// @include      /^https:\/\/(www\.)?bondage-asia\.com\/R\d+\/(Club|\d+)(\/((index|\d+)\.html)?)?$/
 // @homepage     https://github.com/jomshir98/bondage-club-extended#readme
 // @source       https://github.com/jomshir98/bondage-club-extended
 // @downloadURL  https://jomshir98.github.io/bondage-club-extended/bcxLoader.user.js

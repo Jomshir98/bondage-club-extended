8275df3807abbd209bfe7819d4040806846e818d
Jean-Baptiste Emmanuel Zorg <zorgjeanbe@proton.me>
2026-08-24 16:40:47 +0200

    [CHORE] add the new server to the userscripts

--- static_stable/bcxLoader.user.js
+++ static_stable/bcxLoader.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         BCX - Bondage Club Extended (Loader)
 // @namespace    BCX
-// @version      1.0.7
+// @version      1.0.8
 // @description  Loader of Jomshir's "Bondage Club Extended" mod
 // @author       Jomshir98
 // @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
+// @include      /^https:\/\/(www\.)?bondageeurope\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @include      /^https:\/\/(www\.)?bondage-asia\.com\/club\/R\d+(\/((index|\d+)\.html)?)?$/
 // @homepage     https://github.com/jomshir98/bondage-club-extended#readme
 // @source       https://github.com/jomshir98/bondage-club-extended
